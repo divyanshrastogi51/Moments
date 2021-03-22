eaac11c6aed31ffb720b4913a601ba4ca5f18d3d
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-22 09:58:39 +0530

    backend and frontend succesfully connected

--- client/src/testform.js
+++ client/src/testform.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { createPost } from './api';
 
-function testform() {
+const Testform = () => {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    alert(`Submitting Name ${fname}`);
+    const payload = { fname, lname }
+    const res = await createPost(payload);
+    console.log(res.data);
+  };
+
+
   return (
     <div>
-      <h1>Hello</h1>
-      <form action="/action_page.php">
-        <label for="fname">First name:</label>  <br />
-        <input type="text" id="fname" name="fname" value="John" /><br />
-        <label for="lname">Last name:</label> <br />
-        <input type="text" id="lname" name="lname" value="Doe" /><br /><br />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Frirst Name:
+        <input
+            type="text"
+            value={fname}
+            onChange={e => setFname(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:
+        <input
+            type="text"
+            value={lname}
+            onChange={e => setLname(e.target.value)}
+          />
+        </label>
         <input type="submit" value="Submit" />
       </form>
+  );
     </div>
   )
-}
+};
 
-export default testform
+export default Testform
