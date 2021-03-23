dcaf50b45866b2e72febf9f16a835138dfbb820a
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-23 11:45:47 +0530

    backend done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,27 +5,77 @@ import Schema from '../models/Schema.js';
 
 const router = express.Router();
 
+
 export const getPosts = async (req, res) => {
   try {
-    const schema = await Schema.find();
-    res.status(200).json(schema);
+    const postMessages = await Schema.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await Schema.findById(id);
+
+    res.status(200).json(post);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 }
 
 export const createPost = async (req, res) => {
-  const { fname, lname } = req.body;
+  const { title, message, selectedFile, creator, tags } = req.body;
 
-  const newPostMessage = new Schema({ fname, lname })
+  const newSchema = new Schema({ title, message, selectedFile, creator, tags })
 
   try {
-    await newPostMessage.save();
+    await newSchema.save();
 
-    res.status(201).json(newPostMessage);
+    res.status(201).json(newSchema);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
 }
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await Schema.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+  await Schema.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully." });
+}
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+  const post = await Schema.findById(id);
+
+  const updatedPost = await Schema.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+  res.json(updatedPost);
+}
+
 
 export default router;
--- server/models/Schema.js
+++ server/models/Schema.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
-  fname: String,
-  lname: String,
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
   createdAt: {
     type: Date,
     default: new Date(),
