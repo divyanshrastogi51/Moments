bbc0f941ac2d45bc731ffc90a5b4130fb81bdfd6
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-24 20:19:33 +0530

    frontend complete

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 
 import posts from './posts';
+import auth from './auth';
 
-export const reducers = combineReducers({ posts });
+
+export default combineReducers({
+	posts,
+	auth
+});
